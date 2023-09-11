ea07817bc9207efb26b9239940269fbf371b6aaa
hoanln <hoanln@deha-soft.com>
2023-09-11 15:15:11 +0700

    Add auth page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,42 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import '../globals.css';
+import { Inter } from 'next/font/google';
+import Topbar from '@/components/shareds/Topbar';
+import Bottombar from '@/components/shareds/Bottombar';
+import LeftSidebar from '@/components/shareds/LeftSidebar';
+import RightSidebar from '@/components/shareds/RightSidebar';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+    title: 'Threads',
+    description: 'A Next.js 13 Metadata Threads Application'
+};
+
+export default function RootLayout({
+    children
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <ClerkProvider>
+            <html lang='en'>
+                <body className={inter.className}>
+                    <Topbar />
+
+                    <main>
+                        <LeftSidebar />
+
+                        <section className='main-container'>
+                            <div className='w-full max-w-4xl'>{children}</div>
+                        </section>
+
+                        <RightSidebar />
+                    </main>
+
+                    <Bottombar />
+                </body>
+            </html>
+        </ClerkProvider>
+    );
+}
